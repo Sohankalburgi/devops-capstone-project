ecf985e3a66907cdf59276f28aa2f6aeb4d69aef
Sohankalburgi <Sohankalburgi2004@gmail.com>
2026-08-02 07:38:52 -0400

    Fetch Customer Account Details

--- service/routes.py
+++ service/routes.py
@@ -69,7 +69,16 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=['GET'])
+def get_accounts(account_id):
+    """ Reads an account
+    This endpoint will read account based the account id"""
+    app.logger.info("Request to read an account with account id: %s", account_id)
 
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account details with id {account_id} does not exists")
+    return account.serialize(),status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
